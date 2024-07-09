31249fcc38ea2a83151ce1c7d7e674d6603820e4
raketlak <krishrak97@gmail.com>
2024-07-08 23:09:25 -0400

    Update CORS origin and ensure correct route setup

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+
+// Use CORS for your Heroku app
+app.use(cors({ origin: 'https://floating-citadel-66452-32c90ba4ea6a.herokuapp.com' }));
 
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 mongoose.connect(process.env.MONGO_URI_PORTFOLIO)
-    .then( async() => {
+    .then(() => {
         console.log("db connected!");
         app.listen(process.env.PORT || 4000, () => { 
             console.log("Listening on port " + (process.env.PORT || 4000));
