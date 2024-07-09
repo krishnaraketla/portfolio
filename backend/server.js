db660879b68c7d60081db9b37f3b65568f9cd48e
raketlak <krishrak97@gmail.com>
2024-07-08 23:36:22 -0400

    Serve frontend with backend

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,37 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
+const path = require('path');
 
 const app = express();
 
-// Use CORS for your Heroku app
 // Use CORS for development and production
 const allowedOrigins = ['http://localhost:3000', 'https://floating-citadel-66452-32c90ba4ea6a.herokuapp.com'];
 app.use(cors({ origin: allowedOrigins }));
 
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-mongoose.connect(process.env.MONGO_URI_PORTFOLIO)
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI_PORTFOLIO, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("db connected!");
-        app.listen(process.env.PORT || 4000, () => { 
+        app.listen(process.env.PORT || 4000, () => {
             console.log("Listening on port " + (process.env.PORT || 4000));
         });
     })
     .catch((error) => {
         console.log(error);
     });
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Route to serve the frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
+
+// Define a simple route
+app.get('/api', (req, res) => {
+    // res.send('Hello, World!');
+});
