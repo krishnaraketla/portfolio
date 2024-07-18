2dd024862ab3b4791ea3cae87e639c642932ebaa
raketlak <krishrak97@gmail.com>
2024-07-18 14:37:15 -0400

    Hero Section first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import ParticlesComponent from './components/ParticlesComponent';
+import './styles/variable.css';
 
 function App() {
   return (
--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,14 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from "../components/Navbar";
 import ParticlesComponent from "./ParticlesComponent";
 import '../styles/HeroSection.css';
 
+const roles = ["Software Engineer.", "Full Stack Web Developer.", "ML/AI Engineer.","Reader.", "Painter."];
+
 const HeroSection = () => {
+    const [currentRoleIndex, setCurrentRoleIndex] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+
+    useEffect(() => {
+        const textChangeInterval = setInterval(() => {
+            setIsVisible(false);
+            setTimeout(() => {
+                setCurrentRoleIndex(prevIndex => (prevIndex + 1) % roles.length);
+                setIsVisible(true);
+            }, 500); // Update during the invisible phase
+        }, 3000); // Matches the CSS animation time
+
+        return () => clearInterval(textChangeInterval);
+    }, []);
+
     return (
         <>
             <ParticlesComponent />
             <div className="hero">
                 <div className="hero-card"> 
-                    {/* content */}
+                    <h1>Hello, I'm Krishna.<br />I'm a <span className={`changing-text ${!isVisible ? 'invisible' : ''}`}>{roles[currentRoleIndex]}</span></h1>
                 </div>
             </div>
         </>
