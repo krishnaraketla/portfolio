3bb07af54bab2c0e602ec129d6b769acf8853ef8
raketlak <krishrak97@gmail.com>
2024-08-31 21:43:02 -0400

    About me

--- frontend/src/components/PageSection.js
+++ frontend/src/components/PageSection.js
@@ -2,15 +2,21 @@ import React from 'react';
 import '../styles/PageSection.css';
 
 const PageSection = ({ title, image, text, images }) => {
-    console.log(image);
     return (
         <section className="page-section">
-            {image && <div className='page-section-image'> <img src={image} alt='page' /></div>}
-            <div className='page-section-text'>
-                <div className='page-section-title'>{title}</div>
-                <p>{text}</p>
+            {image && (
+                <div className="page-section-image">
+                    <img src={image} alt="page" />
+                </div>
+            )}
+            <div className="page-section-text">
+                <div className="page-section-title">{title}</div>
+                {/* Render each paragraph */}
+                {text.map((paragraph, index) => (
+                    <p key={index}>{paragraph}</p>
+                ))}
                 {images && (
-                    <div className='page-section-images'>
+                    <div className="page-section-images">
                         {images.map((img, index) => (
                             <img key={index} src={img} alt={`artwork-${index}`} />
                         ))}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,10 +8,14 @@ const Home = () => {
         <div className="home">
             <Navbar />
             <HeroSection />
-            <div className="pages-holder">
-                <PageSection 
-                />
-            </div>
+            <PageSection 
+                title="About Me"
+                text={[
+                    "Hi, I'm Krishna! Since September 2024, I've been working as a Software Engineering Intern at LG Energy Solutions Vertech.", 
+                    "When I'm not coding, you'll often find me with a book in hand, indulging in painting, or walking around the streets of Boston shooting film.",
+                    "Outside of my 40 hours per week, I'm building MyBookshelf.com, a platform designed to help users manage their book collections, discover bestsellers, and receive personalized recommendations. This project combines my love for technology and literature, allowing me to create something valuable for fellow book enthusiasts."
+                ]}
+            />
         </div>
     );
 };
