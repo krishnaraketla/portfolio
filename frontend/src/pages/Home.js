d7d130c367c169f1f89f483093cdd0baaba47c28
raketlak <krishrak97@gmail.com>
2024-08-31 21:56:58 -0400

    About section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
                 title="About Me"
                 text={[
                     "Hi, I'm Krishna! Since September 2024, I've been working as a Software Engineering Intern at LG Energy Solutions Vertech.", 
-                    "When I'm not coding, you'll often find me with a book in hand, indulging in painting, or walking around the streets of Boston shooting film.",
+                    "When I'm not coding, you'll often find me with a book in hand (mostly fiction), indulging in painting, or walking around the streets of Boston shooting film.",
                     "Outside of my 40 hours per week, I'm building MyBookshelf.com, a platform designed to help users manage their book collections, discover bestsellers, and receive personalized recommendations. This project combines my love for technology and literature, allowing me to create something valuable for fellow book enthusiasts.",
                     "If you'd like to contribute to any of the projects I'm working on, or think my skills could help with something you're building, feel free to reach out. Let's build something great together!"
                 ]}
